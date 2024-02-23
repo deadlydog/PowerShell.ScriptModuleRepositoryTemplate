d7eaec4dbc1c8d99b371415e6ee3c705c06ff22b
deadlydog <deadlydog@hotmail.com>
2024-02-22 19:29:34 -0600

    Update comment wording in smoke tests a bit

--- deploy/Invoke-SmokeTests.ps1
+++ deploy/Invoke-SmokeTests.ps1
@@ -1,6 +1,6 @@
 # These tests are runs as part of the deployment process to ensure the newly published module is working as expected.
-# These tests run against the installed module, not the source code, so they should not use mocks and are more of a real-world test.
-# Since mocks are not used, we must be careful to not rely on state stored on the machine, such as a module configuration file.
+# These tests run against the installed module, not the source code, so they are more of a real-world test and should not use mocks.
+# Since mocks are not used, be careful to not rely on state stored on the machine, such as a module configuration file.
 # To run these tests on your local machine, see the comments in the BeforeAll block.
 
 BeforeAll {
